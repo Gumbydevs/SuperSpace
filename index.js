1a110248f4db64a6733cc8fb69ca9a1bb2f2399d
Matt Sherman <shermanmatt@me.com>
2025-04-22 13:01:36 -0400

    Add server entry point files for Render deployment

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file is just a wrapper to run the actual server for Render deployment
+console.log('Starting SuperSpace server...');
+require('./server/server.js');
---
+++ server/index.js
@@ -0,0 +1,5 @@
+// Entry point for the SuperSpace server on Render
+// This file just requires and runs server.js
+
+console.log('Starting SuperSpace server from index.js entry point...');
+require('./server.js');
